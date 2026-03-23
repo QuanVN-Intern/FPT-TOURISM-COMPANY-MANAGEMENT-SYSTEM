2d39a984ab383bf32e8b6c3437f3eaa1463c19b1
Baek Hon Nam <namnguyenmta1201@gmail.com>
2026-03-23 17:36:57 +0700

    Update model

---
+++ TOURISM-COMPANY-MANAGEMENT-SYSTEM/Models/TourVehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TOURISM_COMPANY_MANAGEMENT_SYSTEM.Models;
+
+public partial class TourVehicle
+{
+    public int Id { get; set; }
+
+    public int TourId { get; set; }
+
+    public int VehicleId { get; set; }
+
+    public virtual Vehicle Vehicle { get; set; } = null!;
+}
